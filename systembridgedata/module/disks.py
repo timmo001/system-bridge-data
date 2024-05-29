c401b500c061655da25b4054ac09e481b69a23d3
Aidan Timson <aidan@timmo.dev>
2024-05-29 22:17:36 +0100

    Add disks

---
+++ systembridgedata/module/disks.py
@@ -0,0 +1,34 @@
+"""Disks."""
+
+from psutil import disk_io_counters, disk_partitions, disk_usage
+from psutil._common import sdiskio, sdiskpart, sdiskusage
+
+from systembridgeshared.base import Base
+
+
+class Disks(Base):
+    """Disks data."""
+
+    async def get_io_counters(self) -> sdiskio | None:
+        """Disk IO counters."""
+        return disk_io_counters()
+
+    async def get_io_counters_per_disk(self) -> dict[str, sdiskio]:
+        """Disk IO counters per disk."""
+        return disk_io_counters(perdisk=True)
+
+    async def get_partitions(self) -> list[sdiskpart]:
+        """Disk partitions."""
+        return disk_partitions(all=True)
+
+    async def get_usage(self, path: str) -> sdiskusage | None:
+        """Disk usage."""
+        try:
+            return disk_usage(path)
+        except (FileNotFoundError, PermissionError) as error:
+            self._logger.warning(
+                "Error getting disk usage for: %s",
+                path,
+                exc_info=error,
+            )
+            return None
