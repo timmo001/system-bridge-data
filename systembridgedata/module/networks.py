c2ed8a6c69758d08ea9fa6cc3c0c087fe5c958c7
Aidan Timson <aidan@timmo.dev>
2024-05-29 22:13:11 +0100

    Networks

---
+++ systembridgedata/module/networks.py
@@ -0,0 +1,28 @@
+"""Network."""
+
+from psutil import net_connections, net_if_addrs, net_if_stats, net_io_counters
+from psutil._common import sconn, snetio, snicaddr, snicstats
+
+from systembridgeshared.base import Base
+
+
+class Networks(Base):
+    """Networks data."""
+
+    async def get_addresses(
+        self,
+    ) -> dict[str, list[snicaddr]]:
+        """Addresses."""
+        return net_if_addrs()
+
+    async def get_connections(self) -> list[sconn]:
+        """Get connections."""
+        return net_connections("all")
+
+    async def get_io_counters(self) -> snetio:
+        """IO Counters."""
+        return net_io_counters()
+
+    async def get_stats(self) -> dict[str, snicstats]:
+        """Stats."""
+        return net_if_stats()
