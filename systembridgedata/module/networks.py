0f159ff75dfad324c33fff028c289de888d18f4c
Aidan Timson <contact@timmo.xyz>
2024-05-30 18:40:59 +0100

    Fix

--- systembridgedata/module/networks.py
+++ systembridgedata/module/networks.py
@@ -1,7 +1,12 @@
 """Network."""
 
 from psutil import net_connections, net_if_addrs, net_if_stats, net_io_counters
-from psutil._common import sconn, snetio, snicaddr, snicstats
+from systembridgemodels.modules.networks import (
+    NetworkAddress,
+    NetworkConnection,
+    NetworkIO,
+    NetworkStats,
+)
 
 from systembridgeshared.base import Base
 
@@ -11,18 +16,68 @@ class Networks(Base):
 
     def get_addresses(
         self,
-    ) -> dict[str, list[snicaddr]]:
+    ) -> dict[str, list[NetworkAddress]]:
         """Addresses."""
-        return net_if_addrs()
+        data = net_if_addrs()
 
-    def get_connections(self) -> list[sconn]:
+        result = {}
+        for key, value in data.items():
+            result[key] = [
+                NetworkAddress(
+                    address=item.address,
+                    netmask=item.netmask,
+                    broadcast=item.broadcast,
+                    ptp=item.ptp,
+                )
+                for item in value
+            ]
+
+        return result
+
+    def get_connections(self) -> list[NetworkConnection]:
         """Get connections."""
-        return net_connections("all")
+        data = net_connections("all")
 
-    def get_io_counters(self) -> snetio:
+        return [
+            NetworkConnection(
+                fd=item.fd,
+                family=item.family,
+                type=item.type,
+                laddr=str(item.laddr),
+                raddr=str(item.raddr),
+                status=item.status,
+                pid=item.pid,
+            )
+            for item in data
+        ]
+
+    def get_io_counters(self) -> NetworkIO:
         """IO Counters."""
-        return net_io_counters()
+        data = net_io_counters()
 
-    def get_stats(self) -> dict[str, snicstats]:
+        return NetworkIO(
+            bytes_sent=data.bytes_sent,
+            bytes_recv=data.bytes_recv,
+            packets_sent=data.packets_sent,
+            packets_recv=data.packets_recv,
+            errin=data.errin,
+            errout=data.errout,
+            dropin=data.dropin,
+            dropout=data.dropout,
+        )
+
+    def get_stats(self) -> dict[str, NetworkStats]:
         """Stats."""
-        return net_if_stats()
+        data = net_if_stats()
+
+        result = {}
+        for key, value in data.items():
+            result[key] = NetworkStats(
+                isup=value.isup,
+                duplex=str(value.duplex),
+                speed=value.speed,
+                mtu=value.mtu,
+                flags=value.flags.split(",") if value.flags else None,
+            )
+
+        return result
