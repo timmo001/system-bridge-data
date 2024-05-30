8b49f9b9fb7ce7a86eaf9c6638cd6c20b8faec97
timmo001 <timmo001@users.noreply.github.com>
2024-05-30 17:31:28 +0000

    Bump systembridgedata version to 5.0.0.dev4

--- systembridgedata/_version.py
+++ systembridgedata/_version.py
@@ -7,5 +7,5 @@ Provides systembridgedata version information.
 
 from incremental import Version
 
-__version__ = Version("systembridgedata", 5, 0, 0, dev=3)
+__version__ = Version("systembridgedata", 5, 0, 0, dev=4)
 __all__ = ["__version__"]
