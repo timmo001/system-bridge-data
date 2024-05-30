d0e9f2c5405c7db656c32664660742e511fcaa8c
Aidan Timson <contact@timmo.xyz>
2024-05-30 17:55:40 +0100

    Update

--- systembridgedata/module/disks.py
+++ systembridgedata/module/disks.py
@@ -1,7 +1,7 @@
 """Disks."""
 
 from psutil import disk_io_counters, disk_partitions, disk_usage
-from psutil._common import sdiskio, sdiskpart, sdiskusage
+from systembridgemodels.modules.disks import DiskIOCounters, DiskPartition, DiskUsage
 
 from systembridgeshared.base import Base
 
@@ -9,22 +9,65 @@ from systembridgeshared.base import Base
 class Disks(Base):
     """Disks data."""
 
-    def get_io_counters(self) -> sdiskio | None:
+    def get_io_counters(self) -> DiskIOCounters | None:
         """Disk IO counters."""
-        return disk_io_counters()
+        if (data := disk_io_counters()) is None:
+            return None
 
-    def get_io_counters_per_disk(self) -> dict[str, sdiskio]:
+        return DiskIOCounters(
+            read_bytes=data.read_bytes,
+            write_bytes=data.write_bytes,
+            read_count=data.read_count,
+            write_count=data.write_count,
+            read_time=data.read_time,
+            write_time=data.write_time,
+        )
+
+    def get_io_counters_per_disk(self) -> dict[str, DiskIOCounters]:
         """Disk IO counters per disk."""
-        return disk_io_counters(perdisk=True)
+        result: dict[str, DiskIOCounters] = {}
+        if (data := disk_io_counters(perdisk=True)) is None:
+            return result
 
-    def get_partitions(self) -> list[sdiskpart]:
+        for disk, counters in data.items():
+            result[disk] = DiskIOCounters(
+                read_bytes=counters.read_bytes,
+                write_bytes=counters.write_bytes,
+                read_count=counters.read_count,
+                write_count=counters.write_count,
+                read_time=counters.read_time,
+                write_time=counters.write_time,
+            )
+
+        return result
+
+    def get_partitions(self) -> list[DiskPartition]:
         """Disk partitions."""
-        return disk_partitions(all=True)
+        data = disk_partitions(all=True)
 
-    def get_usage(self, path: str) -> sdiskusage | None:
+        return [
+            DiskPartition(
+                device=item.device,
+                mount_point=item.mountpoint,
+                filesystem_type=item.fstype,
+                options=item.opts,
+                max_file_size=item.maxfile,
+                max_path_length=item.maxpath,
+                usage=self.get_usage(item.mountpoint),
+            )
+            for item in data
+        ]
+
+    def get_usage(self, path: str) -> DiskUsage | None:
         """Disk usage."""
         try:
-            return disk_usage(path)
+            data = disk_usage(path)
+            return DiskUsage(
+                total=data.total,
+                used=data.used,
+                free=data.free,
+                percent=data.percent,
+            )
         except (FileNotFoundError, PermissionError) as error:
             self._logger.warning(
                 "Error getting disk usage for: %s",
